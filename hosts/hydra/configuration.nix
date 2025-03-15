6f2886828c904e876664fdb0959740fb8ddae865
edubarr <eduaraujobarros@gmail.com>
2025-03-15 20:49:50 -0300

    add public ssh key

--- hosts/hydra/configuration.nix
+++ hosts/hydra/configuration.nix
@@ -11,4 +11,10 @@
   # Don't change it bro
   system.stateVersion = stateVersion;
 
+  users.users.nixos = {
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA6AS3aaQ9Wga07/7xIbX9397I9wZLMJm2tqx7T8+4g/ eduaraujobarros@gmail.com"
+    ];
+  };
+
 }
