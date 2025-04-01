2f39f852a200756fbc1930aee5cc5e92a27ba825
edubarr <eduaraujobarros@gmail.com>
2025-04-01 18:39:22 -0300

    remove unecessary code

--- modules/nginx.nix
+++ modules/nginx.nix
@@ -3,16 +3,11 @@
     acceptTerms = true;
     defaults = {
       email = "eduaraujobarros@gmail.com";
-      dnsProvider = "cloudflare";  # Match your provider
+      dnsProvider = "cloudflare";
       dnsResolver = "1.1.1.1:53";
       dnsPropagationCheck = true;
       environmentFile = "/var/lib/acme/cloudflare-credentials";
     };
-    # certs."dudu.lat" = {
-    #   domain = "*.dudu.lat";
-    #   extraDomainNames = ["dudu.lat"];  # Include apex domain
-      
-    # };
   };
 
   services.nginx = {
@@ -24,14 +19,6 @@
       enableACME = true;
       acmeRoot = null;
       forceSSL = true;
-      # Listen on IPv6 as well
-      listen = [
-        { addr = "0.0.0.0"; port = 80; }
-        { addr = "0.0.0.0"; port = 443; ssl = true; }
-        { addr = "[::]"; port = 80; }
-        { addr = "[::]"; port = 443; ssl = true; }
-      ];
-      
       locations."/" = {
         proxyPass = "http://127.0.0.1:32400";
         proxyWebsockets = true;
