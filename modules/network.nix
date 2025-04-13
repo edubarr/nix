b6d8794015957a4d06974659d5055cc8e2f7fee3
edubarr <eduaraujobarros@gmail.com>
2025-04-13 12:50:14 -0300

    add tailscale

--- modules/network.nix
+++ modules/network.nix
@@ -1,12 +1,48 @@
+{ pkgs, ... }:
 {
+  environment.systemPackages = [ pkgs.tailscale ];
+
   networking.networkmanager.enable = true;
   networking.firewall={
     enable = true;
     allowedTCPPorts = [ 22 80 443 ];
+    allowedUDPPorts = [ config.services.tailscale.port ];
+
+    trustedInterfaces = [ "tailscale0" ];
   };
 
   networking.tempAddresses = "default";
 
   # Enable the SSH server
   services.openssh.enable = true;
+
+  # Enable tailscale
+  services.tailscale.enable = true;
+
+  systemd.services.tailscale-autoconnect = {
+  description = "Automatic connection to Tailscale";
+
+  # make sure tailscale is running before trying to connect to tailscale
+  after = [ "network-pre.target" "tailscale.service" ];
+  wants = [ "network-pre.target" "tailscale.service" ];
+  wantedBy = [ "multi-user.target" ];
+
+  # set this service as a oneshot job
+  serviceConfig.Type = "oneshot";
+
+  # have the job run this shell script
+  script = with pkgs; ''
+    # wait for tailscaled to settle
+    sleep 2
+
+    # check if we are already authenticated to tailscale
+    status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
+    if [ $status = "Running" ]; then # if so, then do nothing
+      exit 0
+    fi
+
+    # otherwise authenticate with tailscale
+    ${tailscale}/bin/tailscale up -authkey tskey-auth-kpBeLgYwKV11CNTRL-2tpTrJxC4yVydtaFn7CoxV7zrWmQwb4z
+  '';
+};
 }
