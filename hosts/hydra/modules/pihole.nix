f7b514d09a4463d096b428b61da5869364c3e364
edubarr <eduaraujobarros@gmail.com>
2025-11-15 10:10:17 -0300

    Add pihole

--- hosts/hydra/modules/pihole.nix
+++ hosts/hydra/modules/pihole.nix
@@ -4,6 +4,26 @@
     enable = true;
     openFirewallDNS = true;
     openFirewallWebserver = true;
+
+    lists = [
+      {
+        url = "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/adblock/multi.txt";
+        type = "block";
+        enabled = true;
+        description = "Hagezi Normal List";
+      }
+    ];
+  };
+
+  services.pihole-ftl.settings = {
+    dns = {
+      upstreams = [
+        "1.1.1.1"
+        "8.8.8.8"
+        "2606:4700:4700::1111"  # Cloudflare IPv6
+        "2001:4860:4860::8888"  # Google IPv6
+      ];
+    };
   };
 
   services.pihole-web = {
