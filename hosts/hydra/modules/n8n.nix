c00bb072ae4282f5061073c16fc44eddd0c6a8e4
edubarr <eduaraujobarros@gmail.com>
2025-11-26 21:55:06 -0300

    add n8n url

--- hosts/hydra/modules/n8n.nix
+++ hosts/hydra/modules/n8n.nix
@@ -3,5 +3,9 @@
   services.n8n = {
     enable = true;
     openFirewall = true;
+    environment = {
+      WEBHOOK_URL = "https://n8n.edubarr.dev";
+      N8N_HOST = "https://n8n.edubarr.dev";
+    };
   };
 }
