6584346c3dc074120fec7af390e409847574d3c2
edubarr <eduaraujobarros@gmail.com>
2026-03-03 20:56:35 -0300

    Change pihole to docker

--- hosts/hydra/modules/pihole.nix
+++ hosts/hydra/modules/pihole.nix
@@ -1,28 +1,31 @@
 { config, ... }:
 {
-  services.pihole-ftl = {
-    enable = true;
-    openFirewallDNS = true;
-    openFirewallWebserver = true;
-  };
-
-  services.pihole-ftl.settings = {
-    dns = {
-      upstreams = [
-        "1.1.1.1"
-        "8.8.8.8"
-        "2606:4700:4700::1111"  # Cloudflare IPv6
-        "2001:4860:4860::8888"  # Google IPv6
+  virtualisation.oci-containers = {
+    backend = "docker";
+    containers.pihole = {
+      image = "pihole/pihole:latest";
+      environmentFiles = [ "/srv/configs/pihole/.env" ];
+      ports = [
+        "53:53/tcp"
+        "53:53/udp"
+        "8080:80/tcp"
       ];
-      listeningMode = "ALL";
+      volumes = [
+        "/srv/configs/pihole/pihole:/etc/pihole"
+        "/srv/configs/pihole/dnsmasq.d:/etc/dnsmasq.d"
+      ];
     };
   };
 
-  services.pihole-web = {
-    enable = true;
-    ports = [ 8080 ];
-  };
+  networking.firewall.allowedTCPPorts = [ 53 8080 ];
+  networking.firewall.allowedUDPPorts = [ 53 ];
 
   # Set Pi-hole as DNS server
   networking.nameservers = [ "127.0.0.1" ];
+
+  systemd.tmpfiles.rules = [
+    "d /srv/configs/pihole 0755 root root -"
+    "d /srv/configs/pihole/pihole 0755 root root -"
+    "d /srv/configs/pihole/dnsmasq.d 0755 root root -"
+  ];
 }
