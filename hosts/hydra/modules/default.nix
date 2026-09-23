d2106329fea9ad4d8cc040c3edcfc636d80092fb
Eduardo Barros <eduaraujobarros@gmail.com>
2026-09-22 22:04:47 -0300

    feat: add lp hw encode

--- hosts/hydra/modules/default.nix
+++ hosts/hydra/modules/default.nix
@@ -4,6 +4,7 @@
     ./containers
     ./dev-env.nix
     ./file-systems.nix
+    ./intel-gpu.nix
     ./local-packages.nix
     ./network.nix
     ./nginx.nix
---
+++ hosts/hydra/modules/intel-gpu.nix
@@ -0,0 +1,4 @@
+{
+  boot.kernelParams = [ "i915.enable_guc=2" ];
+  hardware.enableRedistributableFirmware = true;
+}
